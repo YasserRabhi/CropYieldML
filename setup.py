1e0e80b1701919d22d5bafc32930b6533ffd5c2d
Yasser Rabhi <yesser2010@gmail.com>
2022-04-25 10:50:46 +0200

    Added Initial files via Upload
    Project Pre Packaging - Just starting

---
+++ setup.py
@@ -0,0 +1,23 @@
+#from setuptools import setup
+#setup(
+#    name='cropyieldpackage',
+#    version='1.0.0',
+#    author='RABHI Yasser',
+#    description='ML crop yield predicition',
+#    long_description='ML Project for crop yield predicition based on GIEC climate change scenarios',
+#    url='https://github.com/YasserRabhi/',
+#    keywords='Machine Learning, tensorflow, python',
+#    python_requires='>=3.7, <4',
+#    install_requires=['pandas','geopandas','tensorflow'],
+#    extras_require={
+#        'test': [],
+#    },
+#    package_data={
+#       'final_data': ['./input.final_data.csv'],
+#    }
+#)
+
+
+from setuptools import setup
+if __name__ == '__main__':
+    setup()
